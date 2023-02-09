add debug to MCTS and fix reward backprop

MCTS.py
@@ -7,6 +7,7 @@ from hex import Hex
 import copy
 from torch import float32, float16, float64
 
+DEBUG = False
 
 class Node():
 
@@ -20,6 +21,7 @@ class Node():
     def expand(self, action_probs):
         for action, prob in enumerate(action_probs):
             if (prob > 0):
+                #TODO: Should check legal instead 
                 new_move = self.gamestate.create_move(action)
                 new_gamestate = self.gamestate.play(move=new_move)
                 self.children[action] = Node(
@@ -28,15 +30,18 @@ class Node():
     def select_child(self, score_func):
         # TODO Is this not just argmax, if so refactor to make it more readable (np.argmax)
         max_score = -float("inf")
-        selected_action = None
         selected_child = None
-        for action, child in self.children.items():
+        for action,child in self.children.items():
             score = score_func(parent=self, child=child)
+            if DEBUG:
+                print(f'\t\tAction: {action}\tUCB: {score}')
             if score > max_score:
-                selected_action = action
                 selected_child = child
+                selected_action = action
                 max_score = score
-        return selected_action, selected_child
+        if DEBUG:
+            print(f'\tSelecting {selected_action}')
+        return selected_child
 
 
 # def UCB(parent, child):
@@ -53,14 +58,15 @@ def UCB(parent, child):
 
 
 def dummy_predict(n: int, _: Gamestate):
-    value = 0.5
     policy = [1/n] * n
-    return value, policy
+    return policy
 
 
 class MCTS():
 
     def __init__(self, game: Game, score_func=UCB, root: Node = None, predict_func=None) -> None:
+        if DEBUG:
+            print(f'Starting MCTS from')
         self.game = game
         if root:
             self.initial_gamestate = root.gamestate
@@ -71,38 +77,42 @@ class MCTS():
             self.initial_gamestate = game.get_initial_position()
             self.current_gamestate = game.get_initial_position()
             self.root = Node(prior=-1, gamestate=self.initial_gamestate)
+        if DEBUG:
+            print(self.initial_gamestate)
+            print(f'With {self.root.visits} previous visits')
+            print()
         self.score_func = score_func
         self.predict_func = predict_func if predict_func else partial(
             dummy_predict, game.move_cardinality)
 
     def normalize_action_probs(self, action_probs, gamestate: Gamestate):
-        # TODO np.norm?
-        norm_action_probs = [prob * mask for prob,
-                             mask in zip(action_probs, gamestate.get_legal_moves())]
-        prob_sum = sum(norm_action_probs)
-        norm_action_probs = [prob / prob_sum for prob in norm_action_probs]
-        return norm_action_probs
+        norm_action_probs = np.array([prob * mask for prob,
+                             mask in zip(action_probs, gamestate.get_legal_moves())])
+        return norm_action_probs / np.sum(norm_action_probs)
 
     def run_simulation(self):
-        # TODO remove constant
-        M = 1
 
         self.current_gamestate = self.initial_gamestate
         node = self.root
         search_path = [node]
 
         # using tree policy to find leaf node
+        if DEBUG:
+            print(f'Starting tree search:')
         while (len(node.children) > 0):  # node has children, already expanded
-            _, node = node.select_child(self.score_func)
+            node = node.select_child(self.score_func)
             search_path.append(node)
             self.current_gamestate = node.gamestate
 
+        if DEBUG: 
+            print(f'The leaf node gamestate:')
+            print(self.current_gamestate)
         # expanding leaf node if not in terminal state
         reward = node.gamestate.reward()
         if reward != None:
-            for s_node in search_path:
-                s_node.value += reward
-                s_node.visits += 1
+            if DEBUG:
+                print(f'Returning early due to reward {reward}')
+            self.increment_reward(search_path=search_path, reward=reward)
             return
 
         # print(node.gamestate)
@@ -113,38 +123,65 @@ class MCTS():
         # print(action_probs)
         norm_action_probs = self.normalize_action_probs(
             action_probs=action_probs, gamestate=node.gamestate)
+        if DEBUG:
+            print(f'Action probabilities for expansion:\n\t{norm_action_probs}')
+            print(f'Expanding...')
         node.expand(action_probs=norm_action_probs)
 
-        # perform M rollouts
         leaf_node = node
-        for _ in range(M):
-            self.current_gamestate = leaf_node.gamestate
-            # TODO constant
-            epsilon = 0.1
-            while reward == None:  # i.e. we are not in a terminal state
-                action_probs = self.predict_func(
-                    self.current_gamestate.get_int_list_representation())
-                norm_action_probs = self.normalize_action_probs(
-                    action_probs=action_probs, gamestate=self.current_gamestate)
-                # select next move in rollout phase
-                choose_random = random.random()
-                move = None
-                if choose_random < epsilon:
-                    true_idx = np.argwhere(
-                        np.array(self.current_gamestate.get_legal_moves()))
-                    random_idx = random.randint(0, len(true_idx) - 1)
-                    move = true_idx[random_idx][0]
-                else:
-                    move = np.argmax(norm_action_probs)
+        self.current_gamestate = leaf_node.gamestate
+        # TODO constant
+        epsilon = 0.1
+        if DEBUG:
+            print(f'Starting rollout')
+        while reward == None:  # i.e. we are not in a terminal state
+            action_probs = self.predict_func(
+                self.current_gamestate.get_int_list_representation()
+                )
+            norm_action_probs = self.normalize_action_probs(
+                action_probs=action_probs, 
+                gamestate=self.current_gamestate
+                )
+            if DEBUG:
+                print(f'\tMove probabilities: \n\t{norm_action_probs}')
+            # select next move in rollout phase
+            choose_random = random.random()
+            move = None
+            if choose_random < epsilon:
+                if DEBUG: 
+                    print(f'\tEpsilon choice')
+                true_idx = np.argwhere(
+                    np.array(self.current_gamestate.get_legal_moves()))
+                random_idx = random.randint(0, len(true_idx) - 1)
+                move = true_idx[random_idx][0]
+            else:
+                move = np.argmax(norm_action_probs)
+            if DEBUG:
+                print(f'\tSelected move: {move}')
+            
 
-                # TODO this can play illegal moves
-                self.current_gamestate = self.current_gamestate.play_move_int(
-                    move_idx=move)
-                reward = self.current_gamestate.reward()
+            self.current_gamestate = self.current_gamestate.play_move_int(
+                move_idx=move
+                )
+            if DEBUG: 
+                print(f'\tNext gamestate:')
+                print(self.current_gamestate)
+            reward = self.current_gamestate.reward()
+        if DEBUG:
+            print(f'Incrementing values')
+        self.increment_reward(search_path=search_path, reward=reward)
 
-            for s_node in search_path:
-                s_node.value += reward
-                s_node.visits += 1
+    def increment_reward(self, search_path : list[Node], reward: int):
+        player_in_root = self.initial_gamestate.get_agent_index()
+        offset = 1 if ( player_in_root == 0 and reward == 1 ) or (player_in_root == 1 and reward == -1) else 0
+        if DEBUG:
+            print(f'Offset: {offset}')
+            print(f'Player: {player_in_root}')
+            print(f'Reward: {reward}')
+        for node in search_path:
+            node.visits += 1
+        for node in search_path[offset::2]:
+            node.value += 1
 
     def run_simulations(self, n: int) -> np.ndarray:
         for _ in range(n):
@@ -169,11 +206,12 @@ class MCTS():
         return action_probs
 
 
+
 if __name__ == "__main__":
     game = Hex(4)
     gs = game.get_initial_position()
     mcts = MCTS(game, score_func=UCB)
-    for i in range(10000):
+    for i in range(1):
         mcts.run_simulation()
 
     print("Best place to start:")