565ca48a3de1d11e9f93396607d83f0ad540a62f
Oskar Jorgensen <oskarjorgensen@gmail.com>
2023-03-24 12:18:16 +0100

    add new constants

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,3 +1,5 @@
+import torch
+
 # 0: no debugging
 # 1: light debugging
 # 2: heavy debugging
@@ -5,16 +7,17 @@ DEBUG_LEVEL = 0
 
 # CONSTANTS IN NEURAL NET TRAINER
 NUM_EPOCHS = 1
-LEARNING_RATE = 0.1
-BATCH_SIZE = 4
+LEARNING_RATE = 0.05
+BATCH_SIZE = 32
+DEVICE = "cpu" # "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
 
 
 # CONSTANTS IN MCTS
 MCTS_EPSILON = 0.2
 GAME_MOVE_EPSILON = 0.2
 SQRT_2 = 1.41
-ROLLOUTS = 1000
+ROLLOUTS = 200
 
 # Multi threading
 M_THREAD = True
-CORES = 8
+CORES = 10
